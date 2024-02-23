51aa2e29b0cec86adf68dbd231d30f2131c87523
rufatjabbarov22 <rufatjabbarov001@gmail.com>
2024-02-23 11:01:44 +0000

    2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,13 @@
-include "holberton.h"
+#include <stdio.h>
+
+
 /**
  * * print_alphabet_x10 - Prints the alphabet 10 times.
  * *
  * * Return: Always 0
  * */
+
+
 void print_alphabet_x10(void)
 {
 
