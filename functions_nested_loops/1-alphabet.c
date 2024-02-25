8c02740299980d7d659fa3c08589366daf5863fb
rufatjabbarov22 <rufatjabbarov001@gmail.com>
2024-02-25 06:18:35 +0000

    alphabet

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -6,13 +6,13 @@
  */
 void print_alphabet(void)
 {
-	    char c = 'a';
-
- while (c <= 'z')
-{
+       	char c = 'a';
+	
+	while (c <= 'z')
+	{
 		    _putchar(c);
 		    c++;
-}
-	    _putchar('\n');
+	}
+       	_putchar('\n');
 
 }
