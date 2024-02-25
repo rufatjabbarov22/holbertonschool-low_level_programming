4eaba992627d52896b7f2b5814673989d0e0897c
rufatjabbarov22 <rufatjabbarov001@gmail.com>
2024-02-25 06:50:36 +0000

    5

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -3,10 +3,9 @@
 /**
  * print_sign - prints the sign of a number
  * @n: the int to check
- *
  * Return: 1 and prints + if n is greater than zero
- *         0 and prints 0 if n is zero
- *        -1 and prints - if n is less than zero
+ * 0 and prints 0 if n is zero
+ * -1 and prints - if n is less than zero
  */
 int print_sign(int n)
 {
@@ -17,12 +16,12 @@ int print_sign(int n)
 	}
 	else if (n == 0)
 	{
-		_putchar('0');
+		_putchar(48);
 		return (0);
 	}
 	else if (n < 0)
 	{
 		_putchar('-');
-		return (-1);
 	}
+	return (-1);
 }
