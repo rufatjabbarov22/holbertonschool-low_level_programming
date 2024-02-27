05c3bbcbc7645ac0c443189436c2a43d54c5aeec
rufatjabbarov22 <rufatjabbarov001@gmail.com>
2024-02-27 13:18:54 +0000

    HOLBERTONSC

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,17 +1,16 @@
 #include "main.h"
 
 /**
-* print_most_numbers - prints numbers except 2 and
-* Return: returns nothing 
-*/
-
+ * print_most_numbers - prints numbers except 2 and
+ * Return: returns nothing
+ */
 void print_most_numbers(void)
 {
-	int x = 0;
+	int x  = 0;
 
 	for (x = 0; x <= 9; x++)
 		if ((x != 2) && (x != 4))
-			_putchar(x = '0');
+			_putchar(x + '0');
 
 	_putchar('\n');
 }
