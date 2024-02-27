130c17f0244736b5f84070dca435783348b06024
rufatjabbarov22 <rufatjabbarov001@gmail.com>
2024-02-27 13:37:53 +0000

    HOLBERTON SCHO

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,25 +1,24 @@
 #include "main.h"
 
 /**
-* main - check the code
-*
-* Return: Always 0.
-*/
-
-int main(void)
+ * more_numbers - Print 10 times the numbers since 0 up to 14
+ * Return: 10 times of the numbers since 0 up to 14
+ */
+void more_numbers(void)
 {
-	int x,y;
+    int x, y;
 
-	for (x = 0; x < 10; x++)
-	{
-		for (y = 0, y <= 14, y++)
-		{
-			if (y > 9)
-			{
-				_putchar((y / 10) + '0');
-			}
-			_putchar((y % 10) + '0';
-					}
-					_putchar('\n');
-					}
-					}
+    for (x = 0; x < 10; x++)
+    {
+        for (y = 0; y <= 14; y++)
+        {
+            if (y > 9)
+            {
+                _putchar((y / 10) + '0');
+            }
+            _putchar((y % 10) + '0');
+        }
+        _putchar('\n');
+    }
+}
+
