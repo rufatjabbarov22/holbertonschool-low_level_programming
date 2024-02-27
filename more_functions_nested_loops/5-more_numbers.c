eee598fac590c812adda346d9bc20dad1b8d0fd0
rufatjabbarov22 <rufatjabbarov001@gmail.com>
2024-02-27 13:32:52 +0000

    HOLBERTON SCH

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* main - check the code
+*
+* Return: Always 0.
+*/
+
+int main(void)
+{
+	int x,y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0, y <= 14, y++)
+		{
+			if (y > 9)
+			{
+				_putchar((y / 10) + '0');
+			}
+			_putchar((y % 10) + '0';
+					}
+					_putchar('\n');
+					}
+					}
