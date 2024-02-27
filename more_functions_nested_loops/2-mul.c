9a7fcfbf28b507137905fa9f5e083d5d8e263f7d
rufatjabbarov22 <rufatjabbarov001@gmail.com>
2024-02-27 12:21:17 +0000

    HOLBERTO

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -2,10 +2,10 @@
 
 /**
  * mul - function that multipliestwo numbers
- * @a: first number 
- * @b: second number
- * Return: returns result
- */
+* @a: first number
+* @b: second number
+* Return: returns result
+*/
 
 int mul(int a, int b)
 {
