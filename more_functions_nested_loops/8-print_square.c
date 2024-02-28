a6f6ace00bdeb1c522b5adb447715c896fc68ab7
rufatjabbarov22 <rufatjabbarov001@gmail.com>
2024-02-28 08:44:34 +0000

    please work

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,27 +1,26 @@
 #include "main.h"
 
 /**
-* print_square - prints a square made of hash symbols.
-* @size: size of the square
+*print_square - prints squares
+*@size: parameter
+*Return:returns nothing
 */
 void print_square(int size)
 {
-    int row, column;
+int inc1, inc2;
+if (size > 0)
+{
+for (inc1 = 0; inc1 < size; inc1++)
+{
+for (inc2 = 0; inc2 < (size - 1); inc2++)
+_putchar('#');
 
-    if (size > 0)
-    {
-        for (row = 0; row < size; row++)
-        {
-            for (column = 0; column < size; column++)
-            {
-                _putchar('#');
-            }
-            _putchar('\n');
-        }
-    }
-    else
-    {
-        _putchar('\n');
-    }
+_putchar('#');
+_putchar('\n');
 }
-
+}
+else
+{
+_putchar('\n');
+}
+}
