3ff6c963646e6d41fccad1ee765eddcab12d1fec
rufatjabbarov22 <rufatjabbarov001@gmail.com>
2024-03-01 10:22:25 +0000

    9

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,23 +1,26 @@
 #include "main.h"
 
 /**
-* _strcpy - Copiar el contenido de uno a otra variable
-* @dest: This is destiny
-* @src: This is the copia
+* _strcpy - Copy the contents of one string to another
+* @dest: Destination string where the copy will be stored
+* @src: Source string to be copied
 *
-* Return: This return copy
+* Return: Pointer to the destination string
 */
-char *_strcpy(char *dest, char *src)
+char *_strcpy(char *dest, const char *src)
 {
-	char *start = dest;
+    char *start = dest;
 
-	while (*src)
-	{
-		*dest = *src;
-		dest++;
-		src++;
-	}
-	*dest = '\n';
+    // Copy characters from source to destination until null terminator is reached
+    while (*src != '\0')
+    {
+        *dest = *src;
+        dest++;
+        src++;
+    }
 
-	return start;
+    // Add null terminator to the destination string
+    *dest = '\0';
+
+    return start;
 }
