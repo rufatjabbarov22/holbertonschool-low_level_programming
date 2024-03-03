71fb4e9a31cfd88717f8b4af3940c90a12a6479c
rufatjabbarov22 <rufatjabbarov001@gmail.com>
2024-03-03 08:37:53 +0000

    6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -3,6 +3,7 @@
 /**
 *puts2 - function that prints every other character of a string,
 *starting with the first character, followed by a new line.
+*@str: this is input String
 */
 void puts2(char *str)
 {
