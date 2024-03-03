96b24ab44077be8db53e72c466f6aaf4164598eb
rufatjabbarov22 <rufatjabbarov001@gmail.com>
2024-03-03 10:45:47 +0000

    Mozart

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -6,22 +6,23 @@
 *
 *Return: address of s
 */
+
 char *leet(char *s)
 {
-   int i, j;
-   char a[] = "aAeEoOtTlL";
-   char b[] = "4433007711";
-
-   for (i = 0; s[i]; i++)
-   {
-       for (j = 0; j < 10; j++)
-       {
-           if (a[j] == s[i])
-           {
-               s[i] = b[j];
-               break;
-           }
-       }
-   }
-   return (s);
+	int i, j;
+	char a[] = "aAeEoOtTlL";
+	char b[] = "4433007711";
+	
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (a[j] == s[i])
+			{
+				s[i] = b[j];
+				break;
+			}
+		}
+	}
+	return (s);
 }
