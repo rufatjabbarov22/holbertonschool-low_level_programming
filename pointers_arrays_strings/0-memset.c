eede95cf3f6ad1114be7db9ab5c31007a02c8d44
rufatjabbarov22 <rufatjabbarov001@gmail.com>
2024-03-05 13:44:01 +0400

    memset

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -6,6 +6,8 @@
 *pointed to by with the constant byte  b
 *@s: target
 *@b: constant byte
+*@n: number of byte
+*Return: returns new value of target
 */
 
 char *_memset(char *s, char b, unsigned int n)
