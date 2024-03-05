1215f690b4403c1cca15ac7aab8e012615ce31a3
rufatjabbarov22 <rufatjabbarov001@gmail.com>
2024-03-05 13:56:43 +0400

    strspn

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+*_strspn - search the number of bytes in the initial
+*segment of s which consist only of bytes from accept
+*@s:reference bytes container
+*
+*Return: returns the number of bytes in the initial
+*segment of s which consist only of bytes from accept
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+				break;
+		}
+		if (!accept[j])
+			break;
+	}
+	return (i);
+}
