8f374781bffcc35e15a7f75998a6d880a303cf0f
rufatjabbarov22 <rufatjabbarov001@gmail.com>
2024-03-09 16:56:01 +0400

    to infinity and beyond, with you

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -7,34 +7,24 @@
 *@argv: array of arguments
 *Return: 0 on success, 1 on failure
 */
+
 int main(int argc, char *argv[])
 {
-    int i, j;
-    long sum = 0;
+	int i, j, sum = 0;
 
-    if (argc < 2)
-    {
-        printf("0\n");
-        return 0;
-    }
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
 
-    for (i = 1; i < argc; i++)
-    {
-        for (j = 0; argv[i][j] != '\0'; j++)
-        {
-            if (j == 0 && (argv[i][j] == '+' || argv[i][j] == '-'))
-                continue;
-            if (argv[i][j] < '0' || argv[i][j] > '9')
-            {
-                printf("Error\n");
-                return 1;
-            }
-        }
-        long num = atol(argv[i]);
-        sum += num;
-    }
-    printf("%ld\n", sum);
-
-    return 0;
+	return (0);
 }
-
