f58f59e6ed91589871fa027bac197a9bcd60103f
rufatjabbarov22 <rufatjabbarov001@gmail.com>
2024-03-09 16:35:31 +0400

    irony man

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*main - prints multiplication
+*of two numbers
+*@argc: number of arguments
+*@argv: array of arguments
+*
+*Return: returns 0
+*/
+
+
+int main(int argc, char *argv[])
+{
+	int i, j, num;
+	if (argc <= 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	i = atoi(argv[1]);
+	j = atoi(argv[2]);
+	num = i *j;
+	printf("%d\n", num);
+	return (0);
+}
