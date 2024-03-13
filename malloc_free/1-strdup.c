8b09b95c0d9e72111f889588bc111ae84ab4f0e6
rufatjabbarov22 <rufatjabbarov001@gmail.com>
2024-03-13 08:02:36 +0000

    a butterfly, string like a bee

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+*_strdup - returns a pointer to a newly allocated space in memory
+*@str: this is my input string
+*
+*Return: pointer of an array of chars
+*/ 
+
+char *_strdup(char *str)
+{
+	int i, len = 0;
+	char *dup;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[len])
+	{
+		len++;
+	}
+	dup = malloc((len + 1) * sizeof(char));
+
+	if (dup == NULL)
+		return (NULL);
+
+	for (i = 0; i <= len; i++)
+	{
+		dup[i] = str[i];
+	}
+	return (dup);
+}
