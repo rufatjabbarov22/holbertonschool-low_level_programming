4663a3f60a6cdb1e1ee7bc154dd5b2aa64257707
rufatjabbarov22 <rufatjabbarov001@gmail.com>
2024-03-17 08:54:46 +0000

    Trust no one

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
+
+/**
+* *malloc_checked - allocate memory with malloc
+*@b:unsigned int type
+*Return: return pointer
+*
+*/
+
+void *malloc_checked(unsigned int b)
+{
+	int *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
+}
---
+++ more_malloc_free/README.md
@@ -0,0 +1,1 @@
+if you're reading this it means at least you have internet, it's not that bad
