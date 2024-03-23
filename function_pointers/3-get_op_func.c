51ecaf72e31c3a3e343fb44c13e6cd63fbe7881e
rufatjabbarov22 <rufatjabbarov001@gmail.com>
2024-03-23 08:53:58 +0000

    get_op_func

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,32 @@
+#include "3-calc.h"
+
+/**
+* *get_op_func - elects the correct operation to perform
+*@s: operation to perform
+*
+*Return: pointer to the correct function
+*/
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{ "+", op_add },
+		{ "-", op_sub },
+		{ "*", op_mul },
+		{ "/", op_div },
+		{ "%", op_mod },
+		{ NULL, NULL }
+	};
+	int i = 0;
+
+	while (ops[i].op != NULL)
+	{
+		if (*(ops[i].op) == *s && s[1] == '\0')
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+	printf("Error\n");
+	exit(99);
+}
