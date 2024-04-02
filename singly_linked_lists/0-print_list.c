4996c9a12ec9f35e267d9fc7d0d8dc1a000d95fc
rufatjabbarov22 <rufatjabbarov001@gmail.com>
2024-04-02 09:58:42 +0000

    0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -11,18 +11,18 @@
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
-	
+
 	while (h != NULL)
 	{
 		if (h->str != NULL)
 			printf("[%d] %s\n", h->len, h->str);
 		else
 			printf("[0] (nil)\n");
-		
+
 		h = h->next;
 		count++;
 	}
-	
-	return count;
+
+	return (count);
 }
 
--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -15,13 +15,12 @@
 
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 /* Function prototypes */
 size_t print_list(const list_t *h);
 
 #endif /* LISTS_H */
-
