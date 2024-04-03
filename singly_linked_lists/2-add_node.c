040d239559ba2cc4795052976b0edd13a44c4724
rufatjabbarov22 <rufatjabbarov001@gmail.com>
2024-04-03 09:03:32 +0000

    addnode

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -13,6 +13,7 @@
 list_t *add_node(list_t **head, const char *str)
 {
     list_t *new_node;
+    unsigned int len = 0;
 
     if (str == NULL)
         return (NULL);
@@ -28,7 +29,10 @@ list_t *add_node(list_t **head, const char *str)
         return (NULL);
     }
 
-    new_node->len = strlen(str);
+    while (str[len] != '\0')
+        len++;
+
+    new_node->len = len;
     new_node->next = *head;
     *head = new_node;
 
