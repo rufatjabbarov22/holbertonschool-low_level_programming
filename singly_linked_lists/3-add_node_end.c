333a970f92b90645daa0d9220f96496bd73b48e4
rufatjabbarov22 <rufatjabbarov001@gmail.com>
2024-04-03 09:17:38 +0000

    addnodeend

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -12,21 +12,21 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-    list_t *new_node, *last_node;
-    unsigned int len = 0;
-
-    if (str == NULL)
-        return (NULL);
-
-    new_node = malloc(sizeof(list_t));
-    if (new_node == NULL)
-        return (NULL);
-
-    new_node->str = strdup(str);
-    if (new_node->str == NULL)
-    {
-        free(new_node);
-        return (NULL);
+	list_t *new_node, *last_node;
+	unsigned int len = 0;
+	
+	if (str == NULL)
+		return (NULL);
+	
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+	
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
     }
 
     new_node->next = NULL;
