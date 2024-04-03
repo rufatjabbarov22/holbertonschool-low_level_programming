5b64b8642a298beefd75a04e3c8af94970f6acfd
Rufat Jabbarov <152645357+rufatjabbarov22@users.noreply.github.com>
2024-04-03 13:35:37 +0400

    Update 3-add_node_end.c

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -25,7 +25,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	new_node->str = strdup(str);
         if (new_node->str == NULL)
 	{
-                free(new_node);
+		free(new_node);
                 return (NULL);
         }
 
