37a5e46663bb064e0c9658316877494eb5ce14d1
rufatjabbarov22 <rufatjabbarov001@gmail.com>
2024-04-03 09:09:48 +0000

    addnode

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -25,17 +25,17 @@ list_t *add_node(list_t **head, const char *str)
 	new_node->str = strdup(str);
 	if (new_node->str == NULL)
 	{
-		free (new_node);
+		free(new_node);
 		return (NULL);
 	}
-	
+
 	while (str[len] != '\0')
 		len++;
-	
+
 	new_node->len = len;
 	new_node->next = *head;
 	*head = new_node;
-	
+
 	return (new_node);
 }
 
