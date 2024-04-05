027982093dbe45ff264a2dba0f775997289b6441
rufatjabbarov22 <rufatjabbarov001@github.com>
2024-04-05 11:25:34 +0000

    42

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -43,12 +43,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		free(new_node);
 		return (NULL);
 	}
-	
+
 	new_node->prev = temp;
 	new_node->next = temp->next;
 	if (temp->next != NULL)
 		temp->next->prev = new_node;
 	temp->next = new_node;
-	
 	return (new_node);
 }
