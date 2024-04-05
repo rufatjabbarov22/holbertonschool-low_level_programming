1192938598d8cead59a0d084d8f2865d5785936b
rufatjabbarov22 <rufatjabbarov001@github.com>
2024-04-05 07:43:48 +0000

    codecodecode

---
+++ singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * print_before_main - Prints a message before main function is executed
+ */
+void __attribute__ ((constructor)) print_before_main()
+{
+    printf("You're beat! and yet, you must allow,\n");
+    printf("I bore my house upon my back!\n");
+}
+
