5dac397c2ff48f5addc17e604bd4daae1732a483
rufatjabbarov22 <rufatjabbarov001@github.com>
2024-04-05 11:18:35 +0000

    h

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -10,22 +10,22 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-    dlistint_t *new_node, *temp = *h;
-    unsigned int i = 0;
-
-    if (h == NULL)
-        return (NULL);
-
-    new_node = malloc(sizeof(dlistint_t));
-    if (new_node == NULL)
-        return (NULL);
-
-    new_node->n = n;
-
-    if (idx == 0)
-    {
-        new_node->prev = NULL;
-        new_node->next = *h;
+	dlistint_t *new_node, *temp = *h;
+	unsigned int i = 0;
+	
+	if (h == NULL)
+		return (NULL);
+	
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	
+	new_node->n = n;
+	
+	if (idx == 0)
+	{
+		new_node->prev = NULL;
+		new_node->next = *h;
         if (*h != NULL)
             (*h)->prev = new_node;
         *h = new_node;
