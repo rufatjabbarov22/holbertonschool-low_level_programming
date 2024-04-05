9934963faaac0a7a4fa83e43daab91b7cd9973e4
rufatjabbarov22 <rufatjabbarov001@github.com>
2024-04-05 08:14:38 +0000

    m

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -9,30 +9,29 @@
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-    dlistint_t *new_node, *last = *head;
-
-    if (head == NULL)
-        return (NULL);
-
-    new_node = malloc(sizeof(dlistint_t));
-    if (new_node == NULL)
-        return (NULL);
-
-    new_node->n = n;
-    new_node->next = NULL;
-
-    if (*head == NULL)
-    {
-        new_node->prev = NULL;
-        *head = new_node;
-        return (new_node);
-    }
-
-    while (last->next != NULL)
-        last = last->next;
-
-    last->next = new_node;
-    new_node->prev = last;
-
-    return (new_node);
+	dlistint_t *new_node, *last = *head;
+	
+	if (head == NULL)
+		return (NULL);
+	
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	
+	new_node->n = n;
+	new_node->next = NULL;
+	
+	if (*head == NULL)
+	{
+		new_node->prev = NULL;
+		*head = new_node;
+		return (new_node);
+	}
+	while (last->next != NULL)
+		last = last->next;
+	
+	last->next = new_node;
+	new_node->prev = last;
+	
+	return (new_node);
 }
