1f4a687fb01bd02c0fdb8cdf0c8b6ebf310207a4
rufatjabbarov22 <rufatjabbarov001@github.com>
2024-04-05 10:38:59 +0000

    6

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 
 /**
- * sum_dlistint - Returns the sum of all the data (n) of a dlistint_t linked list.
- * @head: Pointer to the head of the linked list.
- *
- * Return: Sum of all the data (n) of the linked list.
- */
+*sum_dlistint - Returns the sum of all the data (n) of a dlistint_t linked l.
+* @head: Pointer to the head of the linked list.
+*
+* Return: Sum of all the data (n) of the linked list.
+*/
 int sum_dlistint(dlistint_t *head)
 {
 	int sum = 0;
