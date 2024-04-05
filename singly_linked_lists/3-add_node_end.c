57fc5460b57ead0f0470a5e629ae6edd42b5fbb2
rufatjabbarov22 <rufatjabbarov001@github.com>
2024-04-05 06:53:30 +0000

    newdayoldcode

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -11,23 +11,23 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-    list_t *new_node, *last_node;
-    size_t len = 0;
+	list_t *new_node, *last_node;
+	size_t len = 0;
 
     /* Get the length of the string */
-    while (str[len])
-        len++;
+	while (str[len])
+		len++;
 
     /* Allocate memory for new node */
-    new_node = malloc(sizeof(list_t));
-    if (new_node == NULL)
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
         return (NULL);
 
     /* Initialize new node */
-    new_node->str = strdup(str);
-    if (new_node->str == NULL)
-    {
-        free(new_node);
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
         return (NULL);
     }
     new_node->len = len;
