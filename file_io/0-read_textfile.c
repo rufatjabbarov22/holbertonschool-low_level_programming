4ea1e2952e99e10c3db87e872651305cb97efb1c
rufatjabbarov22 <rufatjabbarov001@gmail.com>
2024-04-06 09:32:17 +0000

    f.her

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -3,42 +3,48 @@
 /**
  * read_textfile - Reads a text file and prints it to the POSIX standard output
  * @filename: The name of the file to read
+ * @letters: The number of letters to read and print
  *
- * Return: The actual number of letters it could read and print
- *         0 if the file can not be opened or read
- *         0 if filename is NULL
- *         0 if write fails or does not write the expected amount of bytes
+ * Return: The actual number of letters read and printed, 0 if failed
  */
-ssize_t read_textfile(const char *filename)
+ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd, bytes_read, bytes_written;
-    char buffer[1024];
+    int fd;
+    ssize_t bytes_read, bytes_written;
+    char *buffer;
 
     if (filename == NULL)
         return (0);
 
-    /* Open the file for reading */
     fd = open(filename, O_RDONLY);
     if (fd == -1)
         return (0);
 
-    /* Read from file */
-    bytes_read = read(fd, buffer, 1024);
-    if (bytes_read == -1)
+    buffer = malloc(sizeof(char) * letters);
+    if (buffer == NULL)
     {
         close(fd);
         return (0);
     }
 
-    /* Write to standard output */
+    bytes_read = read(fd, buffer, letters);
+    if (bytes_read == -1)
+    {
+        free(buffer);
+        close(fd);
+        return (0);
+    }
+
     bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
     if (bytes_written == -1 || bytes_written != bytes_read)
     {
+        free(buffer);
         close(fd);
         return (0);
     }
 
-    /* Clean up and return bytes read */
+    free(buffer);
     close(fd);
-    return (bytes_read);
+
+    return (bytes_written);
 }
