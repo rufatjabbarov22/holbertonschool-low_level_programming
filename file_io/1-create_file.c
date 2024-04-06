06bb6d151512391b1d3702897d4d71df47780fd0
rufatjabbarov22 <rufatjabbarov001@gmail.com>
2024-04-06 09:43:01 +0000

    &

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -10,20 +10,20 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-    int fd, bytes_written;
-    mode_t mode = S_IRUSR | S_IWUSR;
-
-    if (filename == NULL)
-        return (-1);
-
-    fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, mode);
-    if (fd == -1)
-        return (-1);
-
-    if (text_content != NULL)
-    {
-        bytes_written = write(fd, text_content, strlen(text_content));
-        if (bytes_written == -1)
+	int fd, bytes_written;
+	mode_t mode = S_IRUSR | S_IWUSR;
+	
+	if (filename == NULL)
+		return (-1);
+	
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, mode);
+	if (fd == -1)
+		return (-1);
+	
+	if (text_content != NULL)
+	{
+		bytes_written = write(fd, text_content, strlen(text_content));
+		if (bytes_written == -1)
         {
             close(fd);
             return (-1);
