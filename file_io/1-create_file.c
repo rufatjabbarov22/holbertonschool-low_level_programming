b4f18865e1a60bd9434b6610e400dd53e6ef2f80
rufatjabbarov22 <rufatjabbarov001@gmail.com>
2024-04-06 09:39:33 +0000

    undersnow

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h> /* Include <string.h> for strlen */
 
 /**
  * create_file - Creates a file
@@ -9,26 +10,26 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, bytes_written;
-	mode_t mode = S_IRUSR | S_IWUSR;
+    int fd, bytes_written;
+    mode_t mode = S_IRUSR | S_IWUSR;
 
-	if (filename == NULL)
-		return (-1);
+    if (filename == NULL)
+        return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, mode);
-	if (fd == -1)
-		return (-1);
+    fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, mode);
+    if (fd == -1)
+        return (-1);
 
-	if (text_content != NULL)
-	{
-		bytes_written = write(fd, text_content, strlen(text_content));
-		if (bytes_written == -1)
-		{
-			close(fd);
-			return (-1);
-		}
-	}
+    if (text_content != NULL)
+    {
+        bytes_written = write(fd, text_content, strlen(text_content));
+        if (bytes_written == -1)
+        {
+            close(fd);
+            return (-1);
+        }
+    }
 
-	close(fd);
-	return (1);
+    close(fd);
+    return (1);
 }
