77ec59aa0f65dfd6fb42a0926cadf1cde208e058
rufatjabbarov22 <rufatjabbarov001@gmail.com>
2024-04-06 09:24:02 +0000

    sheisnear

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -7,48 +7,47 @@
  *
  * Return: The actual number of letters it could read and print
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd, bytes_read, bytes_written;
-    char *buffer;
+	int fd, bytes_read, bytes_written;
+	char *buffer;
 
-    if (filename == NULL)
-        return (0);
-
-
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-        return (0);
+	if (filename == NULL)
+		return (0);
 
 
-    buffer = malloc(sizeof(char) * letters);
-    if (buffer == NULL)
-    {
-        close(fd);
-        return (0);
-    }
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
 
 
-    bytes_read = read(fd, buffer, letters);
-    if (bytes_read == -1)
-    {
-        free(buffer);
-        close(fd);
-        return (0);
-    }
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
 
 
-    bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-    if (bytes_written == -1 || bytes_written != bytes_read)
-    {
-        free(buffer);
-        close(fd);
-        return (0);
-    }
+	bytes_read = read(fd, buffer, letters);
+	if (bytes_read == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
 
 
-    free(buffer);
-    close(fd);
-    return (bytes_read);
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	if (bytes_written == -1 || bytes_written != bytes_read)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+
+	free(buffer);
+	close(fd);
+	return (bytes_read);
 }
