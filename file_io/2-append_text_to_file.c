07fa9095ee29fe5a4fd90ae7a7584fedc5162a21
rufatjabbarov22 <rufatjabbarov001@gmail.com>
2024-04-06 09:59:16 +0000

    *&^

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -13,7 +13,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	int fd, bytes_written;
 
 	if (filename == NULL)
-        return (-1);
+		return (-1);
 
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
@@ -28,7 +28,7 @@ int append_text_to_file(const char *filename, char *text_content)
 			return (-1);
 		}
 	}
-	
+
 	close(fd);
 	return (1);
 }
