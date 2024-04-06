81a45ddcb8dae8210f1aa7b7ff1d52089ed26753
rufatjabbarov22 <rufatjabbarov001@gmail.com>
2024-04-06 08:09:54 +0000

    and

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -10,8 +10,8 @@ int _putchar(char c);
 
 void print_binary(unsigned long int n)
 {
-    if (n > 1)
+	if (n > 1)
         print_binary(n >> 1);
 
-    _putchar((n & 1) + '0');
+	_putchar((n & 1) + '0');
 }
