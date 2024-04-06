a0a36cd3fcc58ce99a5439c03c2c4719ad38c836
rufatjabbarov22 <rufatjabbarov001@gmail.com>
2024-04-06 08:08:58 +0000

    and

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -1,5 +1,8 @@
 #include "main.h"
 
+
+int _putchar(char c);
+
 /**
 *print_binary - Prints the binary representation of a number.
 *@n: The number to print.
@@ -7,8 +10,8 @@
 
 void print_binary(unsigned long int n)
 {
-	if (n > 1)
-		print_binary(n >> 1);
+    if (n > 1)
+        print_binary(n >> 1);
 
     _putchar((n & 1) + '0');
 }
